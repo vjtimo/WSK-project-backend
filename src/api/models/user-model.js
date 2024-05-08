9c21221d19b9c1cf35a44a3f8f971feba94d36b4
vjtimo <113420766+vjtimo@users.noreply.github.com>
2024-05-08 13:21:20 +0300

    insert customer information into database

--- src/api/models/user-model.js
+++ src/api/models/user-model.js
@@ -12,16 +12,54 @@ const getUserByName = async (name) => {
 };
 
 const addUser = async (user) => {
-  const {tunnus, salasana} = user;
-  const sql = `INSERT INTO kayttaja (tunnus, salasana)
-               VALUES (?, ?)`;
-  const params = [tunnus, salasana];
-  const rows = await promisePool.execute(sql, params);
+  const {
+    tunnus,
+    salasana,
+    email,
+    puhelin,
+    etunimi,
+    sukunimi,
+    katuosoite,
+    postinumero,
+    postitoimipaikka,
+  } = user;
+  try {
+    await promisePool.query('START TRANSACTION');
+    const sql1 = `INSERT INTO kayttaja (tunnus, salasana)
+               VALUES (?, ?);
+                `;
+    const params1 = [tunnus, salasana];
+    const rows1 = await promisePool.query(sql1, params1);
+    const sqlId = `SELECT LAST_INSERT_ID() as user_id;`;
+    const [rows] = await promisePool.query(sqlId);
+    console.log(rows);
+    console.log(rows[0].user_id);
 
-  if (rows[0].affectedRows === 0) {
+    const sql2 = `INSERT INTO asiakas (email, puhelin, etunimi, sukunimi, katuosoite, postinumero, postitoimipaikka, tunnus_id)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
+    const params2 = [
+      email,
+      puhelin,
+      etunimi,
+      sukunimi,
+      katuosoite,
+      postinumero,
+      postitoimipaikka,
+      rows[0].user_id,
+    ];
+    const rows2 = await promisePool.query(sql2, params2);
+    if (rows1[0].affectedRows === 0 || rows2[0].affectedRows === 0) {
+      await promisePool.query('ROLLBACK');
+      return false;
+    }
+    await promisePool.query('COMMIT');
+
+    return {user_id: rows[0].insertId};
+  } catch (e) {
+    console.error('Error in addUser:', e);
+    await promisePool.query('ROLLBACK');
     return false;
   }
-  return {user_id: rows[0].insertId};
 };
 
 export {getUserByName, addUser};
--- src/api/routes/user-route.js
+++ src/api/routes/user-route.js
@@ -5,12 +5,41 @@ import {validationErrors} from '../../middlewares.js';
 import {postUser} from '../controllers/user-controller.js';
 const userRouter = express.Router();
 
-userRouter
-  .route('/register')
-  .post(
-    body('tunnus').trim().isLength({min: 4, max: 20}),
-    body('salasana').trim().isLength({min: 4, max: 30}),
-    validationErrors,
-    postUser
-  );
+userRouter.route('/register').post(
+  body('tunnus')
+    .trim()
+    .isLength({min: 4, max: 20})
+    .withMessage('Tunnuksen tulee olla vähintään 4 merkkiä pitkä'),
+  body('salasana')
+    .trim()
+    .isLength({min: 4, max: 30})
+    .withMessage('Salasanan tulee olla vähintään 4 merkkiä pitkä'),
+  body('email')
+    .isEmail()
+    .withMessage('Sähköpostin tulee olla oikeassa muodossa'),
+  body('puhelin')
+    .isMobilePhone('fi-FI')
+    .withMessage('Puhelinnumeron tulee olla suomalainen puhelinnumero'),
+  body('etunimi')
+    .isAlpha()
+    .isLength({min: 2, max: 30})
+    .withMessage('Etunimi ei saa sisältää numeroita'),
+  body('sukunimi')
+    .isAlpha()
+    .isLength({min: 2, max: 30})
+    .withMessage('Sukunimi ei saa sisältää numeroita'),
+  body('katuosoite')
+    .isLength({min: 5, max: 50})
+    .withMessage('Katuosoitteen tulee olla vähintään 5 merkkiä pitkä'),
+  body('postinumero')
+    .isPostalCode('FI')
+    .withMessage('Postinumeron tulee olla muotoa 12345'),
+  body('postitoimipaikka')
+    .isAlpha()
+    .isLength({min: 2, max: 50})
+    .withMessage('Postitoimipaikka ei saa sisältää numeroita'),
+
+  validationErrors,
+  postUser
+);
 export default userRouter;
