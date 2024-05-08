a77e75ca33d83f5376c63527fd9a69b5664e8ade
mikktur <mikko.silenceri@gmail.com>
2024-05-08 18:20:13 +0300

    added spaced to comments...

--- src/api/controllers/cart-controller.js
+++ src/api/controllers/cart-controller.js
@@ -5,23 +5,23 @@ import {
   addToCart,
 } from '../models/cart-model.js';
 
-//gets cart id by  user id, if the checkCart doesnt find cart by user id then it creates a new empty cart and returns it
+// gets cart id by  user id, if the checkCart doesnt find cart by user id then it creates a new empty cart and returns it
 const getCart = async (req, res, next) => {
   try {
-    const cartId = await checkCart(req.params.id); //params contains the user_id
+    const cartId = await checkCart(req.params.id); // params contains the user_id
 
     if (!cartId) {
-      const newCart = await createNewCart(req.params.id); //creates a new cart and assigns it to the user_id
+      const newCart = await createNewCart(req.params.id); // creates a new cart and assigns it to the user_id
       if (!newCart) {
         const error = new Error('Couldnt create  a new cart');
         error.status = 500;
         next(error);
         return;
       }
-      return res.status(201).json({id: newCart, STORED_ORDERS: []}); //returns the carts id and empty list(the list is kinda unneseccary)
+      return res.status(201).json({id: newCart, STORED_ORDERS: []}); // returns the carts id and empty list(the list is kinda unneseccary)
     }
     const result = await getCartById(cartId);
-    res.json({id: cartId, STORED_ORDERS: result}); //returns cart_id and a list that contains the product ids+quantitys.
+    res.json({id: cartId, STORED_ORDERS: result}); // returns cart_id and a list that contains the product ids+quantitys.
   } catch (e) {
     next(e);
   }
