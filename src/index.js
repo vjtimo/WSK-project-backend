f53afcb93e70b5e92d2a84e133a39f88432bc1b3
mikktur <mikko.silenceri@gmail.com>
2024-05-10 18:51:46 +0300

    trying to get https working

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at https://${hostname}:${port}/`);
 });
