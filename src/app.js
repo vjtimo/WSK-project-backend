d183967eff6e57b6fd37041bed3bff5daaa843e8
mikktur <mikko.silenceri@gmail.com>
2024-05-11 03:49:38 +0300

    testing cors things

--- src/app.js
+++ src/app.js
@@ -11,7 +11,11 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/users', userRouter);
 app.use('/api/pizzas', pizzaRouter);
