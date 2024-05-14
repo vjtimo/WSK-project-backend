47ceac2b91ea8e7519f924045d093e52912fb1d2
mikktur <mikko.silenceri@gmail.com>
2024-05-14 06:36:24 +0300

    removed the cors thingie

--- src/app.js
+++ src/app.js
@@ -11,12 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
-app.use(
-  cors({
-    origin: 'https://users.metropolia.fi',
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/users', userRouter);
 app.use('/api/pizzas', pizzaRouter);
